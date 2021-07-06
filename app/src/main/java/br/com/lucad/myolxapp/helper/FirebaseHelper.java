f17b1f9b80a138ee43972f6e1d3cce463d6f64f0
lucas <lucas.2007s@gmail.com>
2021-07-06 12:02:18 -0300

    SignUp Method

---
+++ app/src/main/java/br/com/lucad/myolxapp/helper/FirebaseHelper.java
@@ -0,0 +1,39 @@
+package br.com.lucad.myolxapp.helper;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class FirebaseHelper {
+
+    private static DatabaseReference firebaseReference;
+    private static FirebaseAuth authReference;
+    private static StorageReference storageReference;
+
+    //retorna referencia do database
+    public static DatabaseReference getFirebaseReference(){
+        if (firebaseReference == null){
+            firebaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+        return firebaseReference;
+    }
+
+    //retorna referencia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAuth(){
+        if (authReference == null){
+            authReference = FirebaseAuth.getInstance();
+        }
+        return authReference;
+    }
+
+    //retorna referencia do FirebaseStorage
+    public static StorageReference getFirebaseStorage(){
+        if (storageReference == null){
+            storageReference = FirebaseStorage.getInstance().getReference();
+        }
+        return storageReference;
+    }
+
+}
---
+++ app/src/main/java/br/com/lucad/myolxapp/ui/activity/MainActivity.java
@@ -0,0 +1,106 @@
+package br.com.lucad.myolxapp.ui.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import br.com.lucad.myolxapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAcess;
+    private EditText emailField, passwordField;
+    private Switch acessType;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeComponents();
+
+        buttonAcessClick();
+
+    }
+
+    private void buttonAcessClick() {
+        buttonAcess.setOnClickListener(v -> {
+            String email = emailField.getText().toString();
+            String password = passwordField.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Preencha os campos!", Toast.LENGTH_LONG).show();
+            } else {
+                if (acessType.isChecked()) {
+                    singUpUser(email, password);
+                } else {
+                    //Login
+                }
+            }
+        });
+    }
+
+    private void singUpUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(
+                email,
+                password
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    signUpException(task);
+                    Toast.makeText(MainActivity.this, signUpException(task), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Cadastro realizado com sucesso", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private String signUpException(@NotNull Task<AuthResult> task) {
+        String errorException = "";
+
+        try {
+            throw Objects.requireNonNull(task.getException());
+        } catch (FirebaseAuthWeakPasswordException e) {
+            errorException = "Digite uma senha mais forte";
+        } catch (FirebaseAuthInvalidCredentialsException e) {
+            errorException = "Digite um email válido";
+        } catch (FirebaseAuthUserCollisionException e) {
+            errorException = "Conta já existe";
+        } catch (Exception e) {
+            errorException = "error : " + e.getMessage();
+            e.printStackTrace();
+        } finally {
+            return errorException;
+        }
+    }
+
+    private void initializeComponents() {
+        buttonAcess = findViewById(R.id.button_acesso);
+        emailField = findViewById(R.id.edit_cadastro_email);
+        passwordField = findViewById(R.id.edit_cadastro_senha);
+        acessType = findViewById(R.id.switch_acesso);
+    }
+
+}
